6cd45fbfb5323fd6624a0e7c546644fc07d87773
Will@Desktop <changwei.cn@gmail.com>
2014-09-24 11:44:35 +0800

    日期控件可用

--- sone/sb-admin-2/js/sb-admin-2.js
+++ sone/sb-admin-2/js/sb-admin-2.js
@@ -26,9 +26,92 @@ $(function() {
         }
     });
 
-   /**显示日期*/
-	$('#reservation').daterangepicker(null, function(start, end, label) {
-		console.log(start.toISOString(), end.toISOString(), label);
-	});
-	 
+    /**显示日期*/
+    //    $('#reservation').daterangepicker();
+
+    var cb = function(start, end, label) {
+        console.log(start.toISOString(), end.toISOString(), label);
+        $('#reportrange span').html(start.format('YYYY-MM-D') + ' - ' + end.format('YYYY-MM-D'));
+        //alert("Callback has fired: [" + start.format('MMMM D, YYYY') + " to " + end.format('MMMM D, YYYY') + ", label = " + label + "]");
+    };
+
+    var optionSet1 = {
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment(),
+        minDate: '01/01/2012',
+        maxDate: '12/31/2020',
+        dateLimit: { days: 60 },
+        showDropdowns: true,
+        showWeekNumbers: true,
+        timePicker: false,
+        timePickerIncrement: 1,
+        timePicker12Hour: true,
+        ranges: {
+            '今天': [moment(), moment()],
+            '昨天': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            '最近 7 天': [moment().subtract(6, 'days'), moment()],
+            '最近 30 天': [moment().subtract(29, 'days'), moment()],
+            '本月': [moment().startOf('month'), moment().endOf('month')],
+            '上月': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        opens: 'left',
+        buttonClasses: ['btn btn-default'],
+        applyClass: 'btn-small btn-primary',
+        cancelClass: 'btn-small',
+        format: 'MM/DD/YYYY',
+        separator: ' to ',
+        locale: {
+            applyLabel: '确定',
+            cancelLabel: '取消',
+            fromLabel: '从',
+            toLabel: '到',
+            customRangeLabel: '自定义',
+            daysOfWeek: ['周日', '周一', '周二', '周三', '周四', '周五','周六'],
+            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+            firstDay: 1
+        }
+    };
+
+    var optionSet2 = {
+        startDate: moment().subtract(7, 'days'),
+        endDate: moment(),
+        opens: 'left',
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    };
+
+    $('#reportrange span').html(moment().subtract(29, 'days').format(' YYYY-MM-D') + ' - ' + moment().format('YYYY-MM-D'));
+
+    $('#reportrange').daterangepicker(optionSet1, cb);
+
+    $('#reportrange').on('show.daterangepicker', function() { console.log("show event fired"); });
+    $('#reportrange').on('hide.daterangepicker', function() { console.log("hide event fired"); });
+    $('#reportrange').on('apply.daterangepicker', function(ev, picker) {
+        console.log("apply event fired, start/end dates are "
+                    + picker.startDate.format('MMMM D, YYYY')
+                    + " to "
+                    + picker.endDate.format('MMMM D, YYYY')
+                   );
+    });
+    $('#reportrange').on('cancel.daterangepicker', function(ev, picker) { console.log("cancel event fired"); });
+
+    $('#options1').click(function() {
+        $('#reportrange').data('daterangepicker').setOptions(optionSet1, cb);
+    });
+
+    $('#options2').click(function() {
+        $('#reportrange').data('daterangepicker').setOptions(optionSet2, cb);
+    });
+
+    $('#destroy').click(function() {
+        $('#reportrange').data('daterangepicker').remove();
+    });
+
+
 });
